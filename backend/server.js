94381aacf184ec90b1d52240c0d20006d26f22c0
A-MOHAN-VAMSI <2400032078@kluniversity.in>
2026-06-15 13:06:49 +0530

    Initiate all

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,14 @@ const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "https://authentication-system-vert.vercel.app",
+        "https://authentication-application-frontend.vercel.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
